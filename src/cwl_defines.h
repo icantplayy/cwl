dcac441b2c7ec4bcdaa89d023db7af5ad8cae2a3
icantplayy <eben.bard@gmail.com>
2024-08-01 23:20:26 -0700

    shlop blob

--- src/cwl_defines.h
+++ src/cwl_defines.h
@@ -1,9 +1,31 @@
+// MIT License
+// 
+// Copyright (c) 2024 icantplay
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 #ifndef _CWL_DEFINES_H_
 #define _CWL_DEFINES_H_
 
 #ifdef _WIN32 // Platform is windows
     #define CWL_PLAT_WIN
-#elif linux // Platform is linux
+#elif __linux__ // Platform is linux
     #define CWL_PLAT_LINUX
 #endif // _WIN32
 
--- src/platform/x11/cwl_window_x11.c
+++ src/platform/x11/cwl_window_x11.c
@@ -1,15 +1,36 @@
+// MIT License
+// 
+// Copyright (c) 2024 icantplay
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 #include "cwl_window.h"
 #include "cwl_defines.h"
 #ifdef CWL_PLAT_LINUX
 #include <X11/Xlib.h>
-
 #include <stdio.h>
 #include <stdlib.h>
 
-static Display*   display   =  {0};
-static Window     window    =  {0};
-static XEvent     event     =  {0};
-static int        screen    =   0;
+static Display* display;
+static XEvent   event;
+static Window   window;
+static int      screen;
 
 CWL_window*
 CWL_Create_Window(int width, int height, const char* title, unsigned int flags)
